e1ecde9c60d6062cf8b26b2ad86b5b0114df80d0
comp-neural-circuits <58776342+comp-neural-circuits@users.noreply.github.com>
2025-10-17 16:17:24 +0200

    Add files via upload

---
+++ T1_1_80/count_followers_2.m
@@ -0,0 +1,71 @@
+groups = 20;
+
+followers_corr_diag = cell(groups, 1);
+followers_corr_subdiag = cell(groups, 1);
+followers_uncorr = cell(groups, 1);
+avg_prob_corr_diag = cell(groups, 1);
+avg_prob_corr_subdiag = cell(groups, 1);
+avg_prob_uncorr = cell(groups, 1);
+all_prob_corr_diag = cell(groups, 1);
+all_prob_corr_subdiag = cell(groups, 1);
+all_prob_uncorr = cell(groups, 1);
+avg_delay_corr_diag = cell(groups, 1);
+avg_delay_corr_subdiag = cell(groups, 1);
+avg_delay_uncorr = cell(groups, 1);
+all_delay_corr_diag = cell(groups, 1);
+all_delay_corr_subdiag = cell(groups, 1);
+all_delay_uncorr = cell(groups, 1);
+
+corr_range = 100;
+
+for i = 1:groups
+        cd(num2str(i))
+        folders = dir();
+        folders = folders(3:end);
+        followers_corr_diag{i} = zeros(length(folders), 1);
+        followers_corr_subdiag{i} = zeros(length(folders), 1);
+        followers_uncorr{i} = zeros(length(folders), 1);
+        for j = 1:length(folders)
+                cd(folders(j).name)
+                load('sequence/delay_and_jitter.mat')
+                source = str2double(folders(j).name);
+                corr_seg = ceil(source/corr_range);
+                followers_corr_diag{i}(j) = sum(index_e >= (1+(corr_seg - 1)*corr_range) & index_e <= corr_seg*corr_range) - 1;
+                if corr_seg == 12
+                        followers_corr_subdiag{i}(j) = sum(index_e <= corr_range);
+                else 
+                        followers_corr_subdiag{i}(j) = sum(index_e >= (1+corr_seg*corr_range) & index_e <= (corr_seg+1)*corr_range);
+                end
+                followers_uncorr{i}(j) = length(index_e) - 1 - followers_corr_diag{i}(j) - followers_corr_subdiag{i}(j);
+                idx_corr_diag = index_e >= (1+(corr_seg - 1)*corr_range) & index_e <= corr_seg*corr_range & index_e ~= source;
+                if corr_seg == 12
+                        idx_corr_subdiag = index_e <= corr_range;
+                        idx_uncorr = index_e <= (corr_seg - 1)*corr_range & index_e > corr_range;
+                else
+                        idx_corr_subdiag = index_e >= (1+corr_seg*corr_range) & index_e <= (corr_seg+1)*corr_range;
+                        idx_uncorr = index_e <= (corr_seg - 1)*corr_range | index_e > (corr_seg+1)*corr_range;
+                end
+                avg_prob_corr_diag{i}(j) = mean(prob_follower_e(idx_corr_diag));
+                avg_prob_corr_subdiag{i}(j) = mean(prob_follower_e(idx_corr_subdiag));
+                avg_prob_uncorr{i}(j) = mean(prob_follower_e(idx_uncorr));
+                all_prob_corr_diag{i} = [all_prob_corr_diag{i}; prob_follower_e(idx_corr_diag)];
+                all_prob_corr_subdiag{i} = [all_prob_corr_subdiag{i}; prob_follower_e(idx_corr_subdiag)];
+                all_prob_uncorr{i} = [all_prob_uncorr{i}; prob_follower_e(idx_uncorr)];
+                avg_delay_corr_diag{i}(j) = mean(med_delay_e(idx_corr_diag));
+                avg_delay_corr_subdiag{i}(j) = mean(med_delay_e(idx_corr_subdiag));
+                avg_delay_uncorr{i}(j) = mean(med_delay_e(idx_uncorr));
+                all_delay_corr_diag{i} = [all_delay_corr_diag{i}; med_delay_e(idx_corr_diag)];
+                all_delay_corr_subdiag{i} = [all_delay_corr_subdiag{i}; med_delay_e(idx_corr_subdiag)];
+                all_delay_uncorr{i} = [all_delay_uncorr{i}; med_delay_e(idx_uncorr)];
+                cd('..');
+        end
+        cd('..')
+end
+
+save('followers_corr_uncorr2.mat', 'followers_corr_diag', 'followers_corr_subdiag', 'followers_uncorr', ...
+        'avg_prob_corr_diag', 'avg_prob_corr_subdiag', 'avg_prob_uncorr', ...
+        'all_prob_corr_diag', 'all_prob_corr_subdiag', 'all_prob_uncorr', ...
+        'avg_delay_corr_diag', 'avg_delay_corr_subdiag', 'avg_delay_uncorr', ...
+        'all_delay_corr_diag', 'all_delay_corr_subdiag', 'all_delay_uncorr');
+
+
---
+++ T1_1_80/template/run_process.jl
@@ -0,0 +1,43 @@
+using Statistics
+
+@with_kw mutable struct the_whole_system
+    dt::Float = 1e-4
+    E::NeuronArray
+    I::NeuronArray
+    EE::Network
+    EI::Network
+    IE::Network
+    II::Network
+    buffer_frames_x::Int = Int(round(1/dt))
+    buffer_frames_w::Int = Int(round(100/dt))
+end
+
+function run_process!(system::the_whole_system, rcdx, rcdw, frame)
+    t = frame * system.dt
+    update_network!(system.EE, system.dt, t)
+    update_network!(system.IE, system.dt, t)
+    update_network!(system.EI, system.dt, t)
+    update_network!(system.II, system.dt, t)
+    update_neurons!(system.E, system.dt, t)
+    update_neurons!(system.I, system.dt, t)
+    MODT_aeif.take_record_x!(frame, system, rcdx, system.buffer_frames_x)
+    MODT_aeif.take_record_w!(frame, system, rcdw, system.buffer_frames_w)
+end
+
+function initialize_system!(system::the_whole_system)
+    initialize_network!(system.EE);
+    initialize_network!(system.EI);
+    initialize_network!(system.IE);
+    initialize_network!(system.II);
+    initialize_neurons!(system.E);
+    initialize_neurons!(system.I);
+end
+
+function initialize_test_system!(system::the_whole_system)
+    initialize_network!(system.EE);
+    initialize_network!(system.EI);
+    initialize_network!(system.IE);
+    initialize_network!(system.II);
+    initialize_test_neurons!(system.E);
+    initialize_test_neurons!(system.I);
+end
